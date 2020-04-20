59905ce99b46c7445bae83cc5a4a5f502919dab3
Kevin Aleman <kaleman@applaudostudios.com>
2020-04-20 16:41:53 -0600

    Rename environment variable to follow gatsby standard

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -36,20 +36,20 @@ export default function Template({
           <meta name="title" content={`${post.frontmatter.title} - ABCDev`}></meta>
           
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content={`${process.env.APP_HOSTNAME}${postPath}`} />
+          <meta property="twitter:url" content={`${process.env.GATSBY_APP_HOSTNAME}${postPath}`} />
           <meta property="twitter:title" content={postTitle} />
           <meta property="twitter:description" content={post.excerpt} />
-          <meta property="twitter:image" content={`${process.env.APP_HOSTNAME}${featuredImgSrc}`} />
+          <meta property="twitter:image" content={`${process.env.GATSBY_APP_HOSTNAME}${featuredImgSrc}`} />
           <meta name="twitter:label1" value="Reading time" />
           <meta name="twitter:data1" value={readingTime} />
           <meta name="twitter:label2" value="Written by" />
           <meta name="twitter:data2" value="Kevin Aleman @kaleman" />
 
           <meta property="og:type" content="website" />
-          <meta property="og:url" content={`${process.env.APP_HOSTNAME}${postPath}`} />
+          <meta property="og:url" content={`${process.env.GATSBY_APP_HOSTNAME}${postPath}`} />
           <meta property="og:title" content={post.frontmatter.title} />
           <meta property="og:description" content={post.excerpt} />
-          <meta property="og:image" content={`${process.env.APP_HOSTNAME}${featuredImgSrc}`} />
+          <meta property="og:image" content={`${process.env.GATSBY_APP_HOSTNAME}${featuredImgSrc}`} />
         </Helmet>
         <BlogPost post={post} headerImg={featuredImgFluid} />
       </div>
